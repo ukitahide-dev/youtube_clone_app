71b19d5bd8b5c0e0eeed3bd22e2d0c50f835ea6e
ukitahide <yukihunji@gmail.com>
2025-12-27 11:32:04 +0900

    fix users/apps.py for render

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -4,9 +4,26 @@
 
 
 from django.apps import AppConfig
-from django.db.utils import OperationalError
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_migrate
+
 import os
+
+
+# DBのマイグレーションが全部終わったあとに、 管理者ユーザーが存在しなければ自動で1人作る
+
+def create_superuser(sender, **kwargs):
+    User = get_user_model()
+
+    # Renderの環境変数を読む
+    email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+    password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+    if not email or not password:
+        return
+
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(email=email, password=password)
 
 
 
@@ -14,20 +31,7 @@ class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
-    # アプリ起動時に1回だけ実行される処理を書く場所（初期化や自動処理）。readyメソッド。
     def ready(self):
-        User = get_user_model()
+        post_migrate.connect(create_superuser, sender=self)
 
-        # Renderの環境変数を読む
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-        if not email or not password:
-            return
-
-        try:
-            if not User.objects.filter(email=email).exists():
-                User.objects.create_superuser(email=email, password=password)
-        except OperationalError:
-            # マイグレーション前はスキップ
-            pass
